8bf25a3827b0cc33782d59ea67e9f76f328c9ba0
Alan-Miller <alan.ashman@gmail.com>
2017-06-23 12:28:22 -0600

    cleaning up code and styles

--- rpg/src/services/postTroop.js
+++ rpg/src/services/postTroop.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
-export function postTroop(type) {
-  console.log('type of recruit', type);
-  return axios.post('http://localhost:3005/defenses/', {type: type})
+export function postTroop(recruit) {
+  return axios.post('http://localhost:3005/defenses/', {recruit: recruit})
   .then(res => {
-    console.log('res.data', res.data);
     return res.data
   })
 }
