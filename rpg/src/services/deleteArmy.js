7b5602e526940722d105bcfb8c932b1a6599ed63
Alan-Miller <alan.ashman@gmail.com>
2017-06-25 04:23:53 -0600

    deleted service files

--- rpg/src/services/deleteArmy.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export function deleteArmy(shortname, id) {
-  return axios.delete('http://localhost:3005/' + shortname + '/' + id)
-  .then(res => {
-    return res.data
-  })
-}
--- rpg/src/services/patchMinion.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export function patchMinion(shortname, id) {
-  return axios.patch('http://localhost:3005/' + shortname + '/minions/' + id, {type: "frog"})
-  .then(res => {
-    return res.status
-  })
-}
